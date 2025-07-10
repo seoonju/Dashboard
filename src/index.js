f734585edada6ee9ab80448cda790e28736894bb
seoonju <tjswn9477@gmail.com>
2025-07-10 15:57:14 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
 import React from "react";
 import { createRoot} from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "App";
 
 // Vision UI Dashboard React Context Provider
 import { VisionUIControllerProvider } from "context";
