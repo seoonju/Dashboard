62fbe992a991f8a9554f6368155a802785a596fd
seoonju <tjswn9477@gmail.com>
2025-07-10 15:36:48 +0900

    Fix: 경로 수정

--- src/App.js
+++ src/App.js
@@ -27,15 +27,15 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Icon from "@mui/material/Icon";
 
 // Vision UI Dashboard React components
-import VuiBox from "./components/VuiBox";
+import VuiBox from "components/VuiBox";
 
 // Vision UI Dashboard React example components
-import Sidenav from "./components/Sidenav";
-import Configurator from "./components/Configurator";
+import Sidenav from "components/Sidenav";
+import Configurator from "components/Configurator";
 
 // Vision UI Dashboard React themes
-import theme from "./assets/theme";
-import themeRTL from "./assets/theme/theme-rtl";
+import theme from "assets/theme";
+import themeRTL from "assets/theme/theme-rtl";
 
 // RTL plugins
 import rtlPlugin from "stylis-plugin-rtl";
@@ -43,10 +43,10 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 
 // Vision UI Dashboard React routes
-import routes from "./routes";
+import routes from "routes";
 
 // Vision UI Dashboard React contexts
-import { useVisionUIController, setMiniSidenav, setOpenConfigurator } from "./context";
+import { useVisionUIController, setMiniSidenav, setOpenConfigurator } from "context";
 
 export default function App() {
   const [controller, dispatch] = useVisionUIController();
